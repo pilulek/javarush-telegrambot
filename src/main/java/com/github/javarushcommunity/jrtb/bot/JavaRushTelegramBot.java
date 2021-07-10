1b58cce153dcb0cf589d2c3adf8d328a00952084
pilulek <60519890+pilulek@users.noreply.github.com>
2021-07-10 15:55:17 +0300

    Jrtb 3 (#17)
    * JRTB-2: added stub telegram bot to the project.
    
    * JRTB-2: added stub telegram bot to the project.
    
    * JRTB-2: added stub telegram bot to the project.
    
    * JRTB-3: added Command pattern for handling Telegram Bot commands
    
    * JRTB-3: added Command pattern for handling Telegram Bot commands
    
    * JRTB-3: added Command pattern for handling Telegram Bot commands
    fix bug
    
    Co-authored-by: Lili Erygina <pilulek@yandex.ru>

--- src/main/java/com/github/javarushcommunity/jrtb/bot/JavaRushTelegramBot.java
+++ src/main/java/com/github/javarushcommunity/jrtb/bot/JavaRushTelegramBot.java
@@ -1,22 +1,45 @@
 package com.github.javarushcommunity.jrtb.bot;
 
+
+import com.github.javarushcommunity.jrtb.command.CommandContainer;
+import com.github.javarushcommunity.jrtb.service.SendBotMessageServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import static com.github.javarushcommunity.jrtb.command.CommandName.NO;
 
 @Component
 public class JavaRushTelegramBot extends TelegramLongPollingBot {
+
+  public static String COMMAND_PREFIX = "/";
+
+
+@Component
+public class JavaRushTelegramBot extends TelegramLongPollingBot {
+
 
 //  @Value("${bot.username")
   @Value("javarush_project_bot")
   private String username;
 
 //  @Value("${bot.token")
+
   @Value("1752666951:AAHVAAI6LMAtvZsLiyj97ttCj2omFxiAnD4")
   private String token;
+
+  private final CommandContainer commandContainer;
+
+  public JavaRushTelegramBot() {
+    this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this));
+  }
+
+
+  @Value("1752666951:AAHVAAI6LMAtvZsLiyj97ttCj2omFxiAnD4")
+  private String token;
+
 
   @Override
   public String getBotUsername() {
@@ -32,6 +55,13 @@ public class JavaRushTelegramBot extends TelegramLongPollingBot {
   public void onUpdateReceived(Update update) {
     if(update.hasMessage() && update.getMessage().hasText()) {
       String message = update.getMessage().getText().trim();
+
+      if(message.startsWith(COMMAND_PREFIX)) {
+        String commandIdentifier = message.split(" ")[0].toLowerCase();
+        commandContainer.retrieveCommand(commandIdentifier).execute(update);
+      } else {
+        commandContainer.retrieveCommand(NO.getCommandName()).execute(update);
+
       String chatId = update.getMessage().getChatId().toString();
 
       SendMessage sm = new SendMessage();
@@ -42,6 +72,7 @@ public class JavaRushTelegramBot extends TelegramLongPollingBot {
         execute(sm);
       } catch (TelegramApiException e) {
         e.printStackTrace();
+
       }
     }
   }
